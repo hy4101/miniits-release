d8103323ef3b49cb33f96c8deea52c7b52e4240f
wq <857454370@qq.com>
2018-04-08 23:59:14 +0800

    Merge branch 'master' of https://gitee.com/haiyuan4101/miniits-release
    # Conflicts:
    #	src/main/resources/templates/admin/components/content/Categorys.ftl

--- src/main/java/com/miniits/base/controller/IndexController.java
+++ src/main/java/com/miniits/base/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.miniits.base.controller;
 
+import com.miniits.base.model.entity.ComponentImage;
 import com.miniits.base.model.entity.Page;
+import com.miniits.base.model.entity.PageComponentAssociate;
 import com.miniits.base.service.PageService;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,9 +10,14 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author: WWW.MINIITS.COM
@@ -46,11 +53,41 @@ public class IndexController {
     @GetMapping("index")
     public String index() {
         StringBuffer html = new StringBuffer();
-        html.append("<div ad=1>1123</div><div ad=1><span>sp</span></div>");
         Page page = pageService.getPage("index", 100000001);
-        String content = "blabla";
-        Document doc = Jsoup.parse(html.toString());
-        Elements element = doc.getElementsByAttributeValue("ad", "1");
+        List<PageComponentAssociate> pageComponentAssociates = page.getPageComponentAssociates().stream().sorted(Comparator.comparing(PageComponentAssociate::getSorts)).collect(Collectors.toList());
+
+//        final int[] i = {1};
+//        pageComponentAssociates.forEach(pca -> {
+//            ComponentImage componentImage = pca.getComponentImage();
+//            if (++i[0] != pca.getLevel()) {
+//                ++i[0];
+//            }
+//            html.append(pca.getComponentImage().getComponentBody());
+//
+//        });
+        int level = 1;
+        boolean bo = false;
+        Document doc = null;
+        for (int i = 0; i < pageComponentAssociates.size(); i++) {
+            ComponentImage componentImage = pageComponentAssociates.get(i).getComponentImage();
+            if (level != pageComponentAssociates.get(i).getLevel()) {
+                ++level;
+            }
+            if (1 != pageComponentAssociates.get(i).getLevel() && StringUtils.isEmpty(html)) {
+
+            }
+            if (pageComponentAssociates.get(i).getLevel() == 1) {
+                html.append(componentImage.getComponentBody());
+                doc = Jsoup.parse(html.toString());
+                continue;
+            } else {
+                String pId = pageComponentAssociates.get(i).getComponentImagePId().getId();
+                String cId = pageComponentAssociates.stream().filter(pca -> pca.getComponentImage().getId().equals(pId)).collect(Collectors.toList()).get(0).getComponentImage().getComponentId();
+                Elements element = doc.getElementsByAttributeValue("componentId", cId);
+                element.append(componentImage.getComponentBody());
+                continue;
+            }
+        }
         return "fd";
     }
 }
