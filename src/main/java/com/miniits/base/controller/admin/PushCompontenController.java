cfa3b744460613069b40117e91a429962c4a4556
wq <857454370@qq.com>
2018-05-17 16:00:33 +0800

    Merge branches 'master' and 'miniits-release-official' of https://gitee.com/haiyuan4101/miniits-release into miniits-release-official
    # Conflicts:
    #	src/main/resources/static/layoutit/index.html

--- src/main/java/com/miniits/base/controller/admin/PushCompontenController.java
+++ src/main/java/com/miniits/base/controller/admin/PushCompontenController.java
@@ -1,6 +1,6 @@
 package com.miniits.base.controller.admin;
 
-import com.alibaba.fastjson.JSON;
+import com.miniits.base.model.dto.AppPushDto;
 import com.miniits.base.model.entity.Component;
 import com.miniits.base.model.entity.User;
 import com.miniits.base.service.ComponentService;
@@ -46,14 +46,23 @@ public class PushCompontenController extends BaseController {
         User currentUser = (User) SecurityUtils.getSubject().getPrincipal();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        MultiValueMap<String, Object> params = new LinkedMultiValueMap<String, Object>();
-        params.add("appContent", toJson(component));
-        params.add("authorId", ObjectUtils.isEmpty(currentUser) ? null : currentUser.getId());
-        params.add("authorName", ObjectUtils.isEmpty(currentUser) ? null : currentUser.getUserName());
-        params.add("appType", APP_TYPE_COMPONENT.toString());
-        params.add("appTypeName", "组件");
-        params.add("remark", remark);
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+//        params.add("app", toJson(component));
+//        params.add("authorId", ObjectUtils.isEmpty(currentUser) ? null : currentUser.getId());
+//        params.add("authorName", ObjectUtils.isEmpty(currentUser) ? null : currentUser.getUserName());
+//        params.add("appType", APP_TYPE_COMPONENT.toString());
+//        params.add("appTypeName", "组件");
+
+
+        AppPushDto appPushDto = new AppPushDto();
+        appPushDto.setComponent(component);
+        appPushDto.setAuthorId(ObjectUtils.isEmpty(currentUser) ? null : currentUser.getId());
+        appPushDto.setAuthorName(ObjectUtils.isEmpty(currentUser) ? null : currentUser.getUserName());
+        appPushDto.setAppType(APP_TYPE_COMPONENT);
+        appPushDto.setAppTypeName("组件");
+        appPushDto.setRemark(remark);
+        params.add("app", toJson(appPushDto));
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(params, headers);
         ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8001/miniits/app/push", requestEntity, String.class);
         return null;
     }
