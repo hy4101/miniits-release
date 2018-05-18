8519d4ee15792ac1a947efc6dbc60b855c9d5519
wq <857454370@qq.com>
2018-05-18 15:16:40 +0800

    Merge branches 'master' and 'miniits-release-official' of https://gitee.com/haiyuan4101/miniits-release into miniits-release-official
    # Conflicts:
    #	src/main/resources/static/layoutit/index.html

--- src/main/java/com/miniits/base/controller/admin/PushCompontenController.java
+++ src/main/java/com/miniits/base/controller/admin/PushCompontenController.java
@@ -1,23 +1,19 @@
 package com.miniits.base.controller.admin;
 
 import com.miniits.base.model.dto.AppPushDto;
+import com.miniits.base.model.entity.AppStore;
 import com.miniits.base.model.entity.Component;
 import com.miniits.base.model.entity.User;
+import com.miniits.base.service.AppStoreServer;
 import com.miniits.base.service.ComponentService;
 import com.miniits.base.utils.BaseController;
+import com.miniits.base.utils.ConvertUtil;
 import com.miniits.base.utils.Result;
 import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import static com.miniits.base.utils.SystemDict.AppType.APP_TYPE_COMPONENT;
 
@@ -34,26 +30,16 @@ import static com.miniits.base.utils.SystemDict.AppType.APP_TYPE_COMPONENT;
 public class PushCompontenController extends BaseController {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private ComponentService componentService;
 
     @Autowired
-    private ComponentService componentService;
+    private AppStoreServer appStoreServer;
 
     @PostMapping("/{id}")
     @ResponseBody
     public Result push(@PathVariable(name = "id") String id, @RequestParam(name = "remark") String remark) throws Exception {
         Component component = componentService.findOne(id);
         User currentUser = (User) SecurityUtils.getSubject().getPrincipal();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-//        params.add("app", toJson(component));
-//        params.add("authorId", ObjectUtils.isEmpty(currentUser) ? null : currentUser.getId());
-//        params.add("authorName", ObjectUtils.isEmpty(currentUser) ? null : currentUser.getUserName());
-//        params.add("appType", APP_TYPE_COMPONENT.toString());
-//        params.add("appTypeName", "组件");
-
-
         AppPushDto appPushDto = new AppPushDto();
         appPushDto.setComponent(component);
         appPushDto.setAuthorId(ObjectUtils.isEmpty(currentUser) ? null : currentUser.getId());
@@ -61,9 +47,9 @@ public class PushCompontenController extends BaseController {
         appPushDto.setAppType(APP_TYPE_COMPONENT);
         appPushDto.setAppTypeName("组件");
         appPushDto.setRemark(remark);
-        params.add("app", toJson(appPushDto));
-        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(params, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8001/miniits/app/push", requestEntity, String.class);
-        return null;
+
+        AppStore appStore = appStoreServer.save(ConvertUtil.toVO(appPushDto, AppStore.class));
+
+        return success(appStore);
     }
 }
